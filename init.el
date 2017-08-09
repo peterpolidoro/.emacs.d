3648e575e89baea1c0a687703f075f96d754f74b
Peter Polidoro <peterpolidoro@gmail.com>
2017-08-09 13:23:52 -0400

    Remove customizations from init.el.

--- init.el
+++ init.el
@@ -1,19 +1,3 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js-indent-level 2)
- '(package-selected-packages
-   (quote
-    (auto-compile flx-ido projectile change-inner expand-region yasnippet typescript-mode highlight-indentation csv-mode json-mode markdown-mode autopair cuda-mode virtualenv auto-complete-nxml cython-mode pov-mode multi-web-mode matlab-mode ein color-theme coffee-mode arduino-mode use-package)))
- '(typescript-indent-level 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
