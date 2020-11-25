11ffe17d6cd5d8b099ba0c0119ab39b1fb443552
Peter Polidoro <peterpolidoro@gmail.com>
2020-11-25 13:12:07 -0500

    Use David Wilson's Emacs.org

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; NOTE: init.el is generated from Emacs.org.  Please edit that file
 ;;       in Emacs and init.el will be generated automatically!
 
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
 ;; Profile emacs startup
 (add-hook 'emacs-startup-hook
 	  (lambda ()
@@ -17,16 +20,12 @@
       auto-save-list-file-prefix (expand-file-name "auto-save-list/.saves-" user-emacs-directory)
       projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" user-emacs-directory))
 
-;; Keep customization settings in a temporary file (thanks Ambrevar!)
+;; Keep customization settings in a temporary file
 (setq custom-file
       (if (boundp 'server-socket-dir)
 	  (expand-file-name "custom.el" server-socket-dir)
 	(expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
 (load custom-file t)
-
-;; You will most likely need to adjust this font size for your system!
-(defvar pjp/default-font-size 120)
-(defvar pjp/default-variable-font-size 120)
 
 ;; Initialize package sources
 (require 'package)
@@ -40,12 +39,32 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-  ;; Initialize use-package on non-Linux platforms
+;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)                ;; if you use any :bind variant
+
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;;(use-package general
+;;  (general-create-definer pjp/leader-key-def
+;;    :keymaps '(normal insert visual emacs)
+;;    :global-prefix "C-."))
+
+;; Thanks, but no thanks
+(setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
@@ -54,16 +73,45 @@
 
 (menu-bar-mode -1)            ; Disable the menu bar
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
+;; Set up the visible bell
+(setq visible-bell t)
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		      treemacs-mode-hook
-		eshell-mode-hook))
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1)
+
+(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(column-number-mode)
+
+;; Enable line numbers for some modes
+(dolist (mode '(text-mode-hook
+                prog-mode-hook
+                conf-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 1))))
+
+;; Override some modes which derive from the above
+(dolist (mode '(org-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(setq large-file-warning-threshold nil)
+
+(setq vc-follow-symlinks t)
+
+(setq ad-redefinition-action 'accept)
+
+(load-theme 'euphoria t t)
+(enable-theme 'euphoria)
+(setq color-theme-is-global t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; You will most likely need to adjust this font size for your system!
+(defvar pjp/default-font-size 120)
+(defvar pjp/default-variable-font-size 120)
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height pjp/default-font-size)
 
@@ -73,33 +121,138 @@
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height pjp/default-variable-font-size :weight 'regular)
 
-(load-theme 'euphoria t t)
-(enable-theme 'euphoria)
-(setq color-theme-is-global t)
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(defun pjp/replace-unicode-font-mapping (block-name old-font new-font)
+  (let* ((block-idx (cl-position-if
+                         (lambda (i) (string-equal (car i) block-name))
+                         unicode-fonts-block-font-mapping))
+         (block-fonts (cadr (nth block-idx unicode-fonts-block-font-mapping)))
+         (updated-block (cl-substitute new-font old-font block-fonts :test 'string-equal)))
+    (setf (cdr (nth block-idx unicode-fonts-block-font-mapping))
+          `(,updated-block))))
 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
+(use-package unicode-fonts
+  :custom
+  (unicode-fonts-skip-font-groups '(low-quality-glyphs))
   :config
-  (setq which-key-idle-delay 1))
+  ;; Fix the font mappings to use the right emoji font
+  (mapcar
+    (lambda (block-name)
+      (pjp/replace-unicode-font-mapping block-name "Apple Color Emoji" "Noto Color Emoji"))
+    '("Dingbats"
+      "Emoticons"
+      "Miscellaneous Symbols and Pictographs"
+      "Transport and Map Symbols"))
+  (unicode-fonts-setup))
+
+(use-package emojify
+  :hook (erc-mode . emojify-mode)
+  :commands emojify-mode)
+
+(setq display-time-format "%l:%M %p %b %y"
+      display-time-default-load-average nil)
+
+(use-package diminish)
+
+(use-package alert
+  :commands alert
+  :config
+  (setq alert-default-style 'notifications))
+
+(use-package super-save
+  :defer 1
+  :diminish super-save-mode
+  :config
+  (super-save-mode +1)
+  (setq super-save-auto-save-when-idle t))
+
+(global-auto-revert-mode 1)
+
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+(setq display-time-world-list
+  '(("America/Los_Angeles" "California")
+    ("America/New_York" "New York")
+    ("Europe/Athens" "Athens")
+    ("Pacific/Auckland" "Auckland")
+    ("Asia/Shanghai" "Shanghai")))
+(setq display-time-world-time-format "%a, %d %b %I:%M %p %Z")
+
+;; Set default connection mode to SSH
+(setq tramp-default-method "ssh")
+
+(use-package hydra
+  :defer 1)
 
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper))
+  :init
+  (ivy-mode 1)
   :config
-  (ivy-mode 1))
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-wrap t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
+
+  ;; Use different regex strategies per completion command
+  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
+  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
+  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
+
+  ;; Set minibuffer height for different commands
+  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
+  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
+  (setf (alist-get 'swiper ivy-height-alist) 15)
+  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
+
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
 
 (use-package ivy-rich
   :init
-  (ivy-rich-mode 1))
+  (ivy-rich-mode 1)
+  :config
+  (setq ivy-format-function #'ivy-format-function-line))
 
 (use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-M-l" . counsel-imenu)
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
   :config
-  (counsel-mode 1))
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+(use-package smex ;; Adds M-x recent command sorting for counsel-M-x
+  :defer 1
+  :after counsel)
+
+(use-package wgrep)
+
+(use-package ivy-posframe
+  :custom
+  (ivy-posframe-width      115)
+  (ivy-posframe-min-width  115)
+  (ivy-posframe-height     10)
+  (ivy-posframe-min-height 10)
+  :config
+  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+  (setq ivy-posframe-parameters '((parent-frame . nil)
+				  (left-fringe . 8)
+				  (right-fringe . 8)))
+  (ivy-posframe-mode 1))
 
 (use-package helpful
   :custom
