0483b9052f0d4f7cc72e507c970d3925892462e7
Peter Polidoro <peterpolidoro@gmail.com>
2020-12-09 14:55:40 -0500

    enter the commit message for your changes. Lines starting

--- init.el
+++ init.el
@@ -481,6 +481,68 @@
 ;; This ends the use-package org-mode block
 )
 
+(defun pjp/lsp-mode-setup ()
+	(setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+	(lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+	:commands (lsp lsp-deferred)
+	:hook (lsp-mode . pjp/lsp-mode-setup)
+	:init
+	(setq lsp-keymap-prefix "s-l")  ;; Or 'C-l', 'C-c l'
+	:config
+	(lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+	:hook (lsp-mode . lsp-ui-mode)
+	:custom
+	(lsp-ui-doc-position 'bottom))
+
+(use-package lsp-treemacs
+	:after lsp)
+
+(use-package lsp-ivy
+	:commands lsp-ivy-workspace-symbol)
+
+(use-package dap-mode
+	:ensure t
+	:commands dap-mode
+	:hook (dap-stopped . (lambda (arg) (call-interactively #'dap-hydra)))
+	:config
+	(dap-mode 1)
+	(require 'dap-ui)
+	(dap-ui-mode 1)
+	(require 'dap-lldb))
+
+(use-package python-mode
+	:ensure t
+	:hook (python-mode . lsp-deferred)
+	:custom
+	;; NOTE: Set these if Python 3 is called "python3" on your system!
+	(python-shell-interpreter "python3")
+	(dap-python-executable "python3")
+	(dap-python-debugger 'debugpy)
+	:config
+	(require 'dap-python))
+
+(use-package pyvenv
+	:config
+	(pyvenv-mode 1))
+
+(use-package company
+	:after lsp-mode
+	:hook (lsp-mode . company-mode)
+	:bind (:map company-active-map
+							("<tab>" . company-complete-selection))
+	(:map lsp-mode-map
+				("<tab>" . company-indent-or-complete-common))
+	:custom
+	(company-minimum-prefix-length 1)
+	(company-idle-delay 0.0))
+
+(use-package company-box
+	:hook (company-mode . company-box-mode))
+
 (use-package magit
 	:commands (magit-status magit-get-current-branch)
 	:diminish magit-auto-revert-mode
@@ -520,7 +582,7 @@
 
 (use-package lsp-mode
 	:commands lsp
-	:hook ((typescript-mode js2-mode web-mode) . lsp)
+	:hook ((python-mode) . lsp)
 	:bind (:map lsp-mode-map
 							("TAB" . completion-at-point)))
 
